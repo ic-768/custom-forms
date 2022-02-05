6263c735c3dfa4c70821a50be99c09001c0173f0
Ioannis Chrysostomakis <ic768@protonmail.com>
2022-02-05 19:29:01 +0200

    Add time input component

--- src/AddInput.tsx
+++ src/AddInput.tsx
@@ -5,14 +5,17 @@ import DropdownInput from "./inputs/DropdownInput";
 import TextInput from "./inputs/TextInput";
 import NumberInput from "./inputs/NumberInput";
 import DateInput from "./inputs/DateInput";
+import TimeInput from "./inputs/TimeInput";
 
 import { getInputIcon, inputsForDropdown } from "./helpers/inputs";
 
 import "./AddInput.scss";
+import { TimePickerValue } from "react-time-picker";
 
 const Edit = () => {
   const [inputType, setInputType] = useState<string | null>(null);
   const [day, setDay] = React.useState<DayValue>(null);
+  const [time, setTime] = React.useState<TimePickerValue>("");
 
   return (
     <div className="edit-panel">
@@ -31,13 +34,20 @@ const Edit = () => {
         </div>
         <div className="edit-panel-option-container">
           {/*Temporarily using this space to code out all the generic inputs for the app*/}
-          <TextInput />
+          <TextInput placeholder="Placeholder for text input" />
         </div>
         <div className="edit-panel-option-container">
-          <NumberInput />
+          <NumberInput placeholder="Placeholder for number input" />
         </div>
         <div className="edit-panel-option-container">
           <DateInput date={day} onChange={setDay} placeholder="Choose a date" />
+        </div>
+        <div className="edit-panel-option-container">
+          <TimeInput
+            value={time}
+            onChange={(e) => setTime(e)}
+            maxDetail="minute"
+          />
         </div>
         <div className="edit-panel-buttons-container">
           <button className="edit-panel-save">Save</button>
---
+++ src/inputs/TimeInput/ITimeInput.tsx
@@ -0,0 +1,11 @@
+import { Dispatch, SetStateAction } from "react";
+import { TimePickerValue } from "react-time-picker";
+
+interface ITimeInput {
+  onChange: Dispatch<SetStateAction<TimePickerValue>>;
+  value: TimePickerValue;
+  className?: string;
+  maxDetail?: "hour" | "minute" | "second";
+}
+
+export default ITimeInput;
