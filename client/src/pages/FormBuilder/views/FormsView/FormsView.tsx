76c07c8e77906b8b0567f3672cedeef60410d12b
Ioannis Chrysostomakis <ic768@protonmail.com>
2022-03-26 19:02:17 +0200

    Style changes to form builder

--- client/src/pages/FormBuilder/views/FormsView/FormsView.tsx
+++ client/src/pages/FormBuilder/views/FormsView/FormsView.tsx
@@ -1,20 +1,33 @@
 import { useState, useEffect } from "react";
 import { TextInput } from "../../../../components/inputs/inputComponents";
 import FormList from "../../components/FormList";
+import { IForm } from "../../resources/shared";
+
 import "./FormsView.scss";
 
-const FormsView = ({ forms, setEditedForm, haveFormsBeenFetched }: any) => {
+interface IFormsView {
+  forms: IForm[];
+  setEditedForm: (form: IForm) => void;
+  haveFormsBeenFetched: boolean;
+}
+
+const FormsView = ({
+  forms,
+  setEditedForm,
+  haveFormsBeenFetched,
+}: IFormsView) => {
   // To filter forms by name
   const [filteredForms, setFilteredForms] = useState(forms);
   const [filterQuery, setFilterQuery] = useState("");
 
   useEffect(() => {
-    setFilteredForms(forms.filter((f: any) => f.name.includes(filterQuery)));
+    setFilteredForms(forms.filter((f: IForm) => f.name.includes(filterQuery)));
   }, [filterQuery, forms]);
 
   return (
     <div className="form-view-container">
       <TextInput
+        placeholder="Search forms ..."
         className="form-view-form-filter"
         onChange={(e) => setFilterQuery(e.target.value)}
         value={filterQuery}
