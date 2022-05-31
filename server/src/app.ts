a657f482ab3c4733de9d964dfbf395e4f4ff6c7b
Ioannis Chrysostomakis <ic768@protonmail.com>
2022-05-31 13:57:44 +0300

    Fix routing on heroku

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,6 +5,7 @@ import { port, client } from "./config";
 import userRouter from "./controllers/users";
 import loginRouter from "./controllers/login";
 import formsRouter from "./controllers/forms";
+import path from "path";
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(cors());
 app.use("/add-user", userRouter);
 app.use("/login", loginRouter);
 app.use("/forms", formsRouter);
+app.get("*", (_, res) => res.sendFile(path.resolve("build", "index.html")));
 
 async function main(): Promise<void> {
   try {
