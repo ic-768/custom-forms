ab24699fbc6d36717df5565e626e292c6034d81f
Ioannis Chrysostomakis <ic768@protonmail.com>
2022-06-24 13:17:53 +0300

    Multiple choice UX improvements

--- client/src/pages/FormBuilder/views/FormEditor/components/ComponentEditor/components/componentModifiers/MultipleChoiceOptions/MultipleChoiceOptions.tsx
+++ client/src/pages/FormBuilder/views/FormEditor/components/ComponentEditor/components/componentModifiers/MultipleChoiceOptions/MultipleChoiceOptions.tsx
@@ -1,4 +1,11 @@
-import { ReactElement, useState } from "react";
+import {
+  ChangeEventHandler,
+  FormEventHandler,
+  MouseEventHandler,
+  ReactElement,
+  useCallback,
+  useState,
+} from "react";
 
 import { TextInput } from "components/inputs/inputComponents";
 import { MultipleChoiceOption } from "components/inputs/inputComponents/MultipleChoiceInput";
@@ -23,51 +30,81 @@ const MultipleChoiceOptions = ({
     onChange(options);
   };
 
+  const onAddOption: FormEventHandler = (e) => {
+    e.preventDefault();
+    updateOptions(multipleChoiceOptions.concat({ label: "" }));
+  };
+
+  const onChangeOption: (
+    index: number
+  ) => ChangeEventHandler<HTMLInputElement> = (index) => (e) => {
+    const updatedOptions = multipleChoiceOptions.map((option, idx) =>
+      index === idx ? { ...option, label: e.target.value } : option
+    );
+    updateOptions(updatedOptions);
+  };
+
+  const onRemoveOption: (
+    index: number
+  ) => MouseEventHandler<HTMLButtonElement> = (index) => (e) => {
+    e.preventDefault();
+    const updatedOptions = multipleChoiceOptions.filter(
+      (_, idx) => index !== idx
+    );
+    updateOptions(updatedOptions);
+  };
+
+  type OptionProps = {
+    option: MultipleChoiceOption;
+    onChange: ChangeEventHandler<HTMLInputElement>;
+    onRemove: MouseEventHandler<HTMLButtonElement>;
+    isLast?: boolean;
+  };
+
+  const Option = useCallback(
+    ({ option, onChange, onRemove, isLast }: OptionProps): ReactElement => (
+      <div className="multiple-choice-options-modifier-option">
+        <TextInput
+          autoFocus={isLast && multipleChoiceOptions.length > 1}
+          onChange={onChange}
+          value={option.label}
+        />
+        <button
+          onClick={onRemove}
+          type="button"
+          className="multiple-choice-options-remove-option-button"
+        >
+          X
+        </button>
+      </div>
+    ),
+    [multipleChoiceOptions.length]
+  );
+
   return (
     <InputContainer
       title="Options"
       subtitle="The selectable options for the multiple choice input"
       component={
-        <div className="multiple-choice-options-modifier-options">
+        <form
+          onSubmit={onAddOption}
+          className="multiple-choice-options-modifier-options"
+        >
           {multipleChoiceOptions.map((o, i) => (
-            <div
+            <Option
+              isLast={i === multipleChoiceOptions.length - 1}
+              onChange={onChangeOption(i)}
+              onRemove={onRemoveOption(i)}
               key={`multiple-choice-options-modifier-input-${i}`}
-              className="multiple-choice-options-modifier-option"
-            >
-              <TextInput
-                onChange={(e): void => {
-                  const updatedOptions = multipleChoiceOptions.map(
-                    (option, idx) =>
-                      i === idx ? { ...option, label: e.target.value } : option
-                  );
-                  updateOptions(updatedOptions);
-                }}
-                value={o.label}
-              />
-              <div
-                onClick={(): void => {
-                  const updatedOptions = multipleChoiceOptions.filter(
-                    (_, idx) => i !== idx
-                  );
-                  updateOptions(updatedOptions);
-                }}
-                className="multiple-choice-options-remove-option-button"
-              >
-                X
-              </div>
-            </div>
+              option={o}
+            />
           ))}
-          <div
+          <input
             className="multiple-choice-options-add-button"
-            onClick={(): void =>
-              setMultipleChoiceOptions(
-                multipleChoiceOptions.concat({ label: "" })
-              )
-            }
-          >
-            +
-          </div>
-        </div>
+            value="+"
+            type="submit"
+          />
+        </form>
       }
     />
   );
