61055ab756c1634b80ffaa9f0fbe0ff477b17eda
Ioannis Chrysostomakis <ic768@protonmail.com>
2022-06-24 16:24:49 +0300

    Refactor DropdownOptions modifier

--- client/src/pages/FormBuilder/views/FormEditor/components/ComponentEditor/components/componentModifiers/DropdownOptions/DropdownOptions.tsx
+++ client/src/pages/FormBuilder/views/FormEditor/components/ComponentEditor/components/componentModifiers/DropdownOptions/DropdownOptions.tsx
@@ -1,4 +1,11 @@
-import { ReactElement, useState } from "react";
+import {
+  ChangeEventHandler,
+  FormEventHandler,
+  MouseEventHandler,
+  ReactElement,
+  useCallback,
+  useState,
+} from "react";
 
 import { TextInput } from "components/inputs/inputComponents";
 import { DropdownOption } from "components/inputs/inputComponents/DropdownInput";
@@ -21,50 +28,79 @@ const DropdownOptions = ({ onChange }: DropdownOptionsProps): ReactElement => {
     onChange(options);
   };
 
+  const onAddOption: FormEventHandler = (e) => {
+    e.preventDefault();
+    updateOptions(dropdownOptions.concat({ label: "", value: "" }));
+  };
+
+  const onChangeOption: (
+    index: number
+  ) => ChangeEventHandler<HTMLInputElement> = (index) => (e) => {
+    const updatedOptions = dropdownOptions.map((option, idx) =>
+      index === idx ? { ...option, label: e.target.value } : option
+    );
+    updateOptions(updatedOptions);
+  };
+
+  const onRemoveOption: (
+    index: number
+  ) => MouseEventHandler<HTMLButtonElement> = (index) => (e) => {
+    e.preventDefault();
+    const updatedOptions = dropdownOptions.filter((_, idx) => index !== idx);
+    updateOptions(updatedOptions);
+  };
+
+  type OptionProps = {
+    option: DropdownOption;
+    onChange: ChangeEventHandler<HTMLInputElement>;
+    onRemove: MouseEventHandler<HTMLButtonElement>;
+    isLast?: boolean;
+  };
+
+  const Option = useCallback(
+    ({ option, onChange, onRemove, isLast }: OptionProps): ReactElement => (
+      <div className="dropdown-options-modifier-option">
+        <TextInput
+          autoFocus={isLast && dropdownOptions.length > 1}
+          onChange={onChange}
+          value={option.label}
+        />
+        <button
+          type="button"
+          onClick={onRemove}
+          className="dropdown-options-remove-option-button"
+        >
+          X
+        </button>
+      </div>
+    ),
+    [dropdownOptions.length]
+  );
+
   return (
     <InputContainer
       title="Options"
       subtitle="The selectable options for the dropdown input"
       component={
-        <div className="dropdown-options-modifier-options">
+        <form
+          onSubmit={onAddOption}
+          className="dropdown-options-modifier-options"
+        >
           {dropdownOptions.map((o, i) => (
-            <div
+            <Option
+              isLast={i === dropdownOptions.length - 1}
+              onChange={onChangeOption(i)}
+              onRemove={onRemoveOption(i)}
               key={`dropdown-options-modifier-input-${i}`}
-              className="dropdown-options-modifier-option"
-            >
-              <TextInput
-                onChange={(e): void => {
-                  const updatedOptions = dropdownOptions.map((option, idx) =>
-                    i === idx ? { ...option, label: e.target.value } : option
-                  );
-                  updateOptions(updatedOptions);
-                }}
-                value={o.label}
-              />
-              <div
-                onClick={(): void => {
-                  const updatedOptions = dropdownOptions.filter(
-                    (_, idx) => i !== idx
-                  );
-                  updateOptions(updatedOptions);
-                }}
-                className="dropdown-options-remove-option-button"
-              >
-                X
-              </div>
-            </div>
+              option={o}
+            />
           ))}
-          <div
+          <input
             className="dropdown-options-add-button"
-            onClick={(): void =>
-              setDropdownOptions(
-                dropdownOptions.concat({ label: "", value: "" })
-              )
-            }
-          >
-            +
-          </div>
-        </div>
+            value="+"
+            type="submit"
+          />
+        </form>
       }
     />
   );
